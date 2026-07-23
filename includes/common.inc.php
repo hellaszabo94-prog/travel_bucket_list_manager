e0d1c0c83e399c81ad893d3d609edbe39ebbeede
Hella Szabo <szabo.hella94@gmail.com>
2026-07-23 18:05:32 +0200

    add a comment to common.inc.php file

--- includes/common.inc.php
+++ includes/common.inc.php
@@ -1,9 +1,17 @@
 <?php
+
+// This function displays the incoming data when TESTOPERATION is active.
+
 function test(mixed $incoming):void {
+
 	if(TESTOPERATION) {
-		echo('<pre class="ta">');
+
+		echo('<pre class="test">');
+
 		print_r($incoming);
+
 		echo('</pre>');
+
 	}
 }
 ?>
