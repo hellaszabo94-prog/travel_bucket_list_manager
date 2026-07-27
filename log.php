ffdf7620017915bc6117669f40924335cd96a9ed
Hella Szabo <szabo.hella94@gmail.com>
2026-07-27 22:23:38 +0200

    add html to the log.php

---
+++ log.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="de">
+    <head>
+        <title>Login</title>
+        <meta charset="utf_8">
+        <link rel="stylesheet" href="css/stylesheet.css">
+    </head>
+    <body>
+    <h1>Meinen Reisemanager</h1>
+    <form method="post">
+        <label>
+            Emailadresse:
+            <input type="email" name="E">
+        </label>
+        <label>
+            Password:
+            <input type="password" name="PWD">
+        </label>
+        <br>
+        <input type="submit" value="Login" name="logButton">
+        <input type="submit" value="Registrierung" name="regButton">
+    </form>
+    </body>
+</html>
