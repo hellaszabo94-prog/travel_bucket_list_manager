a2c428f87b6143509ab2f7db3890012d862b4eb8
Hella Szabo <szabo.hella94@gmail.com>
2026-07-27 22:26:51 +0200

    add html to the reg.php

---
+++ reg.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="de">
+    <head>
+        <title>Registrirung</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/stylesheet.css">
+    </head>
+    <body>
+    <h1>Meinen Reisemanager</h1>
+    <h2>Registrierung</h2>
+    
+    <form method="post">
+        <fieldset>
+            <legend>Pflichtdaten</legend>
+            <label>
+                Emailadresse:
+                <input type="email" name="E">
+            </label>
+            <br>
+            <br>
+            <label>
+                Password (bitte mindestens acht Zeihnen):
+                <input type="password" name="PWD">
+            </label>
+            <br>
+            <br>
+            <label>
+                Password 2:
+                <input type="password" name="PWD2">
+            </label>
+        </fieldset>
+        <br>
+        <fieldset>
+            <legend>Weitere Daten</legend>
+            <label>
+                Vorname:
+                <input type="text" name="VorN">
+            </label>
+            <br>
+            <br>
+            <label>
+                Nachname:
+                <input type="text" name="NachN" >
+            </label>
+            <br>
+            <br>
+            <label>
+                Geburtsdatum:
+                <input type="date" name="GebD">
+            </label>
+        </fieldset>
+        <br>
+        <input type="submit" value="Registrierung" name="regButton">
+        <input type="submit" value="Login" name="logButton">
+    </form>
+    </body>
+</html>
