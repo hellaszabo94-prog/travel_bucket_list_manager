f651d35b6fa0b6f8d94da395adfcc59cf787eb41
Hella Szabo <szabo.hella94@gmail.com>
2026-07-28 17:02:04 +0200

    add the DB query function

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -13,10 +13,30 @@ function dbConnect():mysqli {
 		}
 		else {
 			header("Location: errors/dbconnect.html");
+            exit;
 		}
 	}
 	
 	return $conn;
+}
+
+// query from database
+
+function dbQuery(mysqli $conn, string $sql):mysqli_result|bool {
+	try {
+		$antwort = $conn->query($sql);
+	}
+	catch(Exception $e) {
+		if(TESTOPERATION) {
+			test($e);
+			die("Error in the Query");
+		}
+		else {
+			header("Location: errors/dbquery.html");
+		}
+	}
+	
+	return $antwort;
 }
 
 
