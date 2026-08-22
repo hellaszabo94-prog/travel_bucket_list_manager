c7581b7b76995bb7b8ff80a1f4d1f53ea3cbec59
Hella Szabo <szabo.hella94@gmail.com>
2026-08-22 17:28:41 +0200

    add base tailwind layout styles

--- dashboard.php
+++ dashboard.php
@@ -18,16 +18,18 @@ require ("includes/auth.inc.php");
         <link rel="stylesheet" href="css/tailwind.css">
         <link rel="stylesheet" href="css/stylesheet.css">
     </head>
-    <body>
-    <h1 class="text-5xl font-bold text-blue-700">Travel Bucket List Manager</h1>
-    <h2>Welcome to your personal Travel Bucket List Manager, <?= htmlspecialchars($_SESSION["Firstname"], ENT_QUOTES, "UTF-8") ?>!</h2>
-    <nav>
-		<ul>
-	    	<li><a href="dashboard.php">Home</a></li>
-			<li><a href="add_destination.php">Add a new destination</a></li>
-			<li><a href="my_destinations.php">My Destinations</a></li>
-            <li><a href="logout.php">Log out</a></li>
-		</ul>
-    </nav>
+    <body class="bg-sky-100 text-slate-950 min-h-screen">
+        <main class="max-w-6xl mx-auto px-4">  
+            <h1 class="text-5xl font-bold">Travel Bucket List Manager</h1>
+            <h2>Welcome to your personal Travel Bucket List Manager, <?= htmlspecialchars($_SESSION["Firstname"], ENT_QUOTES, "UTF-8") ?>!</h2>
+            <nav>
+                <ul>
+                    <li><a href="dashboard.php">Home</a></li>
+                    <li><a href="add_destination.php">Add a new destination</a></li>
+                    <li><a href="my_destinations.php">My Destinations</a></li>
+                    <li><a href="logout.php">Log out</a></li>
+                </ul>
+            </nav>
+        </main>
     </body>
 </html>
